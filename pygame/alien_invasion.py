be68b8a0c467828a1af6cec8094c86e7892352cc
Judy <judy.zhu@learnosity.com>
2020-06-14 20:35:05 +1000

    [FEATURE] delete legacy bullets and limit bullets number

--- pygame/alien_invasion.py
+++ pygame/alien_invasion.py
@@ -22,8 +22,15 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -53,8 +60,9 @@ class AlienInvasion:
             sys.exit()
 
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
