db2eda314eb0e1db80fdbcf33d5508523882ad33
Judy <judy.zhu@learnosity.com>
2020-06-14 14:24:02 +1000

    [FEATURE] move to right

--- pygame/alien_invasion.py
+++ pygame/alien_invasion.py
@@ -19,13 +19,20 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-                
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
